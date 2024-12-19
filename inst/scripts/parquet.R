3eefe22c05864d9fe7241361bc8dc8535fc8f65b
sdgamboa <samuel.gamboa.tuz@gmail.com>
2024-12-19 16:29:58 -0500

    Update code with parquet files

--- inst/scripts/parquet.R
+++ inst/scripts/parquet.R
@@ -1,12 +1,12 @@
-library(duckdb)
-library(dplyr)
 
-## Parquet files
+library(duckdb)
+library(duckplyr)
+
 storageBaseURL <- Sys.getenv("CMDDUCKDB")
 dataTypes <- c(
     relab = "metaphlan_bugs",
-    markAb = "marker_abundances",
-    markPr = "marker_presences"
+    mrkAb = "marker_abundances",
+    mrkPr = "marker_presences"
 )
 readParquetCommands <- paste0(
     "create view ", names(dataTypes), " as select * from ",
@@ -18,45 +18,39 @@ con <- dbConnect(duckdb::duckdb(), ":memory:")
 dbExecute(con, "install httpfs;")
 dbExecute(con, "load httpfs;")
 
-tim <- system.time({
-    for (i in seq_along(dataTypes)) {
-        dbExecute(con, readParquetCommands[i])
-    }
-})
-## Running these three took about three minutes (not the first time)
+for (cmd in readParquetCommands) dbExecute(con, cmd)
 
 dbListTables(con)
+   
+## This takes a lot of time, maybe is better to just
+## use SQL queries
+relab <- as_duckplyr_tibble(dplyr::tbl(con, "relab"))
+mrkPr <- as_duckplyr_tibble(dplyr::tbl(con, "mrkPr"))
+mrkAb <- as_duckplyr_tibble(dplyr::tbl(con, "mrkAb"))
+
+res1 <- relab |>
+    select(sample_id) |>
+    # group_by(sample_id) |>
+    summarise(.by = sample_id, bug_count = n()) |>
+    arrange(desc(bug_count)) |>
+    head(10)
+res1
+
+res2 <- relab |>
+    filter(sample_id == "SAMN02334067") |>
+    select(sample_id, clade_name, relative_abundance) |>
+    arrange(desc(relative_abundance)) -> metaphlan_bugs_sample
+res2
+
+head(mrkAb)
 
 ## This one took about 4 seconds (already cached)
-dbGetQuery(con, paste("select * from", names(dataTypes)[1], "limit 5;"))
+# dbGetQuery(con, paste("select * from", names(dataTypes)[1], "limit 5;"))
 
 ## This one took about two minutes (already cached)
-dbGetQuery(con, paste("select * from", names(dataTypes)[2], "limit 5;"))
+# dbGetQuery(con, paste("select * from", names(dataTypes)[2], "limit 5;"))
 
-    ## This oe workout took about
-dbGetQuery(con, paste("select * from", names(dataTypes)[3], "limit 5;"))
-
-
-    
-    
-    
-## This step seems to take a while
-relab <- tbl(con, "relab")
-
-# metaphlan_bugs |>
-#     select(sample_id) |>
-#     group_by(sample_id) |>
-#     summarise(bug_count = n()) |>
-#     arrange(desc(bug_count)) |>
-#     head(10)
-# 
-# metaphlan_bugs |>
-#     filter(sample_id == "SAMN02334067") |>
-#     select(sample_id, clade_name, relative_abundance) |>
-#     arrange(desc(relative_abundance)) -> metaphlan_bugs_sample
-# 
-# head(metaphlan_bugs_sample, 50)
-
-
+## This oe workout took about
+# dbGetQuery(con, paste("select * from", names(dataTypes)[3], "limit 5;"))
 
 dbDisconnect(con)
