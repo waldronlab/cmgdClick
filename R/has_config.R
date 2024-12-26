0fe687dc435a9946baed60eb130a5d71f7538fe6
LiNk-NY <marcel.ramos@sph.cuny.edu>
2024-12-26 15:26:29 -0500

    add doc for has_config

--- R/has_config.R
+++ R/has_config.R
@@ -1,3 +1,13 @@
+#' Check if the user has a configuration file
+#'
+#' @description The function checks if the user has an ODBC configuration file
+#' or a general configuration file. These files are used to connect via ODBC
+#' driver either with an `.odbc.ini` file or a general configuration file
+#' `config.yml`. For details, see [clickhouse_connect()].
+#'
+#' @return A logical vector indicating if the user has an ODBC configuration or
+#'   a general configuration file.
+#'
 #' @examples
 #' has_config()
 #'
@@ -6,6 +16,6 @@ has_config <- function() {
     home <- Sys.getenv("HOME")
     c(
         ODBC = file.exists(file.path(home, ".odbc.ini")),
-        config = file.exists(file.path(home, ".config.yml"))
+        config = file.exists(file.path(home, "config.yml"))
     )
 }
