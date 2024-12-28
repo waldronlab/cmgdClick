304ffd493c35ced7bb2cc678db1a2db72f5bb779
LiNk-NY <marcel.ramos@sph.cuny.edu>
2024-12-28 15:47:29 -0500

    add valid_config and rename to config.Rd

--- R/has_config.R
+++ R/has_config.R
@@ -1,16 +1,22 @@
-#' Check if the user has a configuration file
+#' @name config
 #'
-#' @description The function checks if the user has an ODBC configuration file
-#' or a general configuration file. These files are used to connect via ODBC
-#' driver either with an `.odbc.ini` file or a general configuration file
-#' `config.yml`. For details, see [clickhouse_connect()].
+#' @title Check and validate the user configuration file
+#'
+#' @description These are utitlities to check whether the user has an ODBC
+#'   configuration file or a general configuration file. These files are used to
+#'   connect via ODBC driver either with an `.odbc.ini` file or a general
+#'   configuration file `config.yml` as used by `config` R package. For details,
+#'   see [clickhouse_connect()]. `valid_config` checks if the general
+#'   configuration file has the required fields for connecting to the Clickhouse
+#'   server.
 #'
 #' @return A logical vector indicating if the user has an ODBC configuration or
-#'   a general configuration file.
+#'   a general configuration file (`has_config`) or a valid `config.yml` file
+#'   (`valid_config`).
 #'
 #' @examples
 #' has_config()
-#'
+#' valid_config()
 #' @export
 has_config <- function() {
     home <- Sys.getenv("HOME")
@@ -18,4 +24,24 @@ has_config <- function() {
         ODBC = file.exists(file.path(home, ".odbc.ini")),
         config = file.exists(file.path(home, "config.yml"))
     )
+}
+
+#' @rdname config
+#'
+#' @export
+valid_config <- function() {
+    config <- file.path(Sys.getenv("HOME"), "config.yml")
+    if (!file.exists(config))
+        stop("No configuration file found in '~/config.yml'")
+    BiocBaseUtils::checkInstalled("yaml")
+    clist <- yaml::read_yaml(config)
+    if (!identical("default" ,names(clist)))
+        stop("No 'default' configuration found in '~/config.yml'")
+
+    cfields <- c("host", "user", "password", "port", "db")
+    found <- cfields %in% names(clist[["default"]])
+    notfound <- paste0(cfields[!found], collapse = ", ")
+    if (!all(found))
+        stop("Missing fields in config.yml: ", notfound)
+    TRUE
 }
